f60130b01bfa9c3d4090f203140fbf86c2d4f3b8
Rehan Shah <flyrehan786@gmail.com>
2023-03-24 16:44:03 +0500

    creating core-table component. implementing dynamic headers/columns and record filtering.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  headers = [
+    { id:1, key: 'id'},
+    { id:2, key: 'name'},
+    { id:3, key: 'age'},
+  ];
+  columns = [
+    { id: 1, name: 'rehanz', age: 27 },
+    { id: 2, name: 'rehany', age: 27 },
+    { id: 3, name: 'rehanx', age: 27 },
+  ]
   title = 'app';
 }
--- frontend/src/app/components/core/table/table.component.ts
+++ frontend/src/app/components/core/table/table.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-table',
+  selector: 'core-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
-
+  @Input() title: string = '';
+  @Input() headers: any[] = [];
+  @Input() columns: any[] = [];
+  BACKUP: any[] = [];
   constructor() { }
-
   ngOnInit(): void {
+    this.BACKUP = JSON.parse(JSON.stringify(this.columns));
   }
-
+  filter(keyword: string) {
+    if(keyword.length > 0) {
+      this.columns = this.BACKUP.filter(x => {
+        let flag = false;
+        this.headers.forEach((header) => {
+          const value = x[header.key];
+          console.log(value)
+          if (value && value.toString().toLowerCase().includes(keyword.toLowerCase())) {
+            console.log('trueeee')
+            flag = true;
+          }
+        });
+        console.log('--- true')
+        console.log(flag)
+        return flag;
+      })
+    }
+    else this.columns = JSON.parse(JSON.stringify(this.BACKUP));
+  }
 }
