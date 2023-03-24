11ed086ff3da0d9280a65535f8c9dea87c97bd40
Rehan Shah <flyrehan786@gmail.com>
2023-03-24 15:07:34 +0500

    defining components.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { StudentsComponent } from './components/pages/students/students.component';
+import { TeachersComponent } from './components/pages/teachers/teachers.component';
+import { CertificatesComponent } from './components/pages/certificates/certificates.component';
+import { ReportsComponent } from './components/pages/reports/reports.component';
+import { SettingsComponent } from './components/pages/settings/settings.component';
+import { LoginComponent } from './components/pages/auth/login/login.component';
+import { RegisterComponent } from './components/pages/auth/register/register.component';
+import { UsersComponent } from './components/pages/auth/users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    StudentsComponent,
+    TeachersComponent,
+    CertificatesComponent,
+    ReportsComponent,
+    SettingsComponent,
+    LoginComponent,
+    RegisterComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
