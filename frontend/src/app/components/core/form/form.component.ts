1f7572ce51d5c96d045b8d671edf55a956887fbd
Rehan Shah <flyrehan786@gmail.com>
2023-04-05 02:00:15 +0500

    start working on dynamic form building.

--- frontend/src/app/components/core/form/form.component.ts
+++ frontend/src/app/components/core/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'core-form',
@@ -6,9 +7,31 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-  @Input() config; 
-  @Output() onSubmit = new EventEmitter();
-  constructor() { }
-  ngOnInit(): void {
+  // @Input() config; 
+  // @Output() onSubmit = new EventEmitter();
+
+  @Input() formData: any;
+  form: FormGroup;
+  constructor(private fb: FormBuilder) { }
+  ngOnInit() {
+    this.form = this.fb.group({});
+    for (let control of this.formData.controls) {
+      let validators = [];
+      for (let validator of control.validators) {
+        switch (validator.name) {
+          case 'required':
+            validators.push(Validators.required);
+            break;
+          case 'minLength':
+            validators.push(Validators.minLength(validator.value));
+            break;
+          case 'maxLength':
+            validators.push(Validators.maxLength(validator.value));
+            break;
+          // add more validators as needed
+        }
+      }
+      this.form.addControl(control.name, this.fb.control(control.value, validators));
+    }
   }
 }
