12dbf9d6f17419329cc8d24fcb3aaaba8c02e2da
Rehan Shah <flyrehan786@gmail.com>
2023-04-08 16:14:38 +0500

    code refactoring.

--- frontend/src/app/components/core/form/form.component.ts
+++ frontend/src/app/components/core/form/form.component.ts
@@ -22,22 +22,39 @@ interface IControl {
 export class FormComponent implements OnInit {
   config: IControl[] = [
     {
-      key: 'username', defaultValue: '', 
+      key: 'username', defaultValue: '',
       validators:
         [
-          { key: 'required', value: 'required', message: 'Username should be required' },
-          { key: 'email', value: 'email', message: 'Username should be minimun 10 characters long' },
-          { key: 'maxLength', value: '10', message: 'Username should be minimun 10 characters long' },
-          { key: 'pattern', value: 'rehan', message: 'Username should be minimun 10 characters long' }
+          { key: 'required', value: 'required', message: 'username should be required' },
+          {
+            key: 'email',
+            value: 'email',
+            message: 'email'
+          },
+          {
+            key: 'maxLength',
+            value: '10',
+            message: 'maxLength'
+          },
+          {
+            key: 'minLength',
+            value: '10',
+            message: 'minLength'
+          },
+          {
+            key: 'pattern',
+            value: 'rehan',
+            message: 'pattern'
+          }
         ]
     },
     {
-      key: 'password', defaultValue: '', 
+      key: 'password', defaultValue: '',
       validators:
-      [
-        { key: 'required', value: 'required', message: 'Password should be required' },
-        { key: 'email', value: 'email', message: 'Password should be minimun 10 characters long' }
-      ]
+        [
+          { key: 'required', value: 'required', message: 'Password should be required' },
+          { key: 'email', value: 'email', message: 'Password should be minimun 10 characters long' }
+        ]
     },
   ];
   form: FormGroup;
@@ -49,22 +66,22 @@ export class FormComponent implements OnInit {
       let controlValidators: ValidatorFn[] = [];
       for (let j = 0; j < control.validators.length; j++) {
         const validator = control.validators[j];
-        if(validator.key === 'required') {
+        if (validator.key === 'required') {
           controlValidators.push(Validators.required);
         }
-        else if(validator.key === 'maxLength') {
+        else if (validator.key === 'maxLength') {
           controlValidators.push(Validators.maxLength(+validator.value));
         }
-        else if(validator.key === 'minLength') {
+        else if (validator.key === 'minLength') {
           controlValidators.push(Validators.minLength(+validator.value));
         }
-        else if(validator.key === 'email') {
+        else if (validator.key === 'email') {
           controlValidators.push(Validators.email);
         }
-        else if(validator.key === 'pattern') {
+        else if (validator.key === 'pattern') {
           controlValidators.push(Validators.pattern(validator.value));
         }
-        else {}
+        else { }
       }
 
       controls[control.key] = new FormControl('', controlValidators)
