8ffa0290774d4bf26cf59375700be3c839a42244
Rehan Shah <flyrehan786@gmail.com>
2023-04-12 05:13:59 +0500

    updating app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+// PAGES
+import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { CertificatesComponent } from './components/pages/certificates/certificates.component';
 import { ReportsComponent } from './components/pages/reports/reports.component';
@@ -11,17 +15,17 @@ import { LoginComponent } from './components/pages/auth/login/login.component';
 import { RegisterComponent } from './components/pages/auth/register/register.component';
 import { UsersComponent } from './components/pages/auth/users/users.component';
 import { HomeComponent } from './components/pages/home/home.component';
-import { TableComponent } from './components/core/table/table.component';
-import { RouterModule } from '@angular/router';
-import { FormComponent } from './components/core/form/form.component';
-import { CommonModule } from '@angular/common';
-import { DetailsComponent } from './components/core/details/details.component';
-import { DragAndDropComponent } from './components/core/drag-and-drop/drag-and-drop.component';
-import { TabComponent } from './components/core/tab/tab.component';
 import { StudentsComponent } from './components/pages/students/students/students.component';
 import { StudentDetailComponent } from './components/pages/students/student-detail/student-detail.component';
 import { TeacherDetailComponent } from './components/pages/teachers/teacher-detail/teacher-detail.component';
 import { TeachersComponent } from './components/pages/teachers/teachers/teachers.component';
+// CORE.
+import { TableComponent } from './components/core/table/table.component';
+import { FormComponent } from './components/core/form/form.component';
+import { DetailsComponent } from './components/core/details/details.component';
+import { DragAndDropComponent } from './components/core/drag-and-drop/drag-and-drop.component';
+import { TabComponent } from './components/core/tab/tab.component';
+
 @NgModule({
   declarations: [
     AppComponent,
