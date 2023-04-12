ed02ce246e8bce417a2e59e15266557b6ba7800c
Rehan Shah <flyrehan786@gmail.com>
2023-04-12 05:03:58 +0500

    start working on details component.

--- frontend/src/app/components/core/details/details.component.ts
+++ frontend/src/app/components/core/details/details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-details',
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
-
+  @Input() id: string;
+  @Input() backToListRoute: string; 
+  @Input() data: string;
+  
   constructor() { }
 
   ngOnInit(): void {
