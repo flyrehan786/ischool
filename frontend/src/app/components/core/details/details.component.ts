ffb231a5fc74fe9b0b1bae25dbbca870de812ee0
Rehan Shah <flyrehan786@gmail.com>
2023-04-12 17:44:44 +0500

    implementing a basic UI for core-details component.

--- frontend/src/app/components/core/details/details.component.ts
+++ frontend/src/app/components/core/details/details.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-details',
+  selector: 'core-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
@@ -9,7 +9,13 @@ export class DetailsComponent implements OnInit {
   @Input() id: string;
   @Input() backToListRoute: string; 
   @Input() data: string;
-  
+  obj = {
+    id: '1',
+    name: 'Rehan Shah',
+    cnic: '15402-69990899',
+    remarks: 'I am a full stack js-developer.'
+
+  }
   constructor() { }
 
   ngOnInit(): void {
