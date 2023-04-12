95fabb0221bcff413134ff3717cf1e0c8d462a3e
Rehan Shah <flyrehan786@gmail.com>
2023-04-13 00:57:35 +0500

    adding backend project.

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,17 @@
-console.log('Server')
+const express = require("express");
+const config = require("config");
+const app = express();
+
+require("./startup/logging")();
+require("./startup/cors")(app);
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
+require("./startup/validation")();
+
+const port = process.env.PORT || config.get("port");
+const server = app.listen(port, () =>
+ console.log(`Listening on port ${port}...`)
+);
+
+module.exports = server;
