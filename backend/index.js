b8966d332ab5fb5c76f2ec1dc93ea870f5ef1d31
Rehan Shah <flyrehan786@gmail.com>
2023-04-13 01:04:50 +0500

    removing unnecessary backend - code

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,7 @@
 const express = require("express");
-const config = require("config");
 const app = express();
 
-require("./startup/logging")();
-require("./startup/cors")(app);
-require("./startup/routes")(app);
-require("./startup/db")();
-require("./startup/config")();
-require("./startup/validation")();
-
-const port = process.env.PORT || config.get("port");
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () =>
  console.log(`Listening on port ${port}...`)
 );
