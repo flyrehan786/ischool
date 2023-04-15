e5e1e1d27d32055cc492b04d7735389953fa8bed
Rehan Shah <flyrehan786@gmail.com>
2023-04-16 00:03:54 +0500

    updating version, defining biolerplate code for auth service, add api-endpoint to environment file.

--- frontend/src/app/services/auth/auth.service.ts
+++ frontend/src/app/services/auth/auth.service.ts
@@ -4,6 +4,10 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-
   constructor() { }
+  logIn() {}
+  logOut() {}
+  isloggedIn() {}
+  getLoggedInUser() {
+  }
 }
