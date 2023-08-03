d2d4a781f6473d4b6bbdaf2bd84e43b10c7906ee
Rehan Shah <flyrehan786@gmail.com>
2023-08-04 00:58:12 +0500

    updating auth service.

--- frontend/src/app/services/auth/auth.service.ts
+++ frontend/src/app/services/auth/auth.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor() { }
-  logIn() {}
+  constructor(
+    private route: ActivatedRoute) { 
+  }
+
+  login() {
+    let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+    localStorage.setItem('returnUrl', returnUrl);
+    // ..    
+  }
   logOut() {}
   isloggedIn() {}
   getLoggedInUser() {
