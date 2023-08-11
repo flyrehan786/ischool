24300d0594818514cc9fdea88c3618e2ce9c47b7
Rehan Shah <flyrehan786@gmail.com>
2023-08-11 12:43:51 +0500

    code cleaning.

--- backend/services/mysql.js
+++ backend/services/mysql.js
@@ -5,18 +5,13 @@ let pool = mysql2.createPool({
     password: "root",
     database: "school_db"
 });
-// Connect to the database
 function checkConnection() {
-    // Acquire a connection from the pool
     pool.getConnection((err, connection) => {
         if (err) {
             console.error('Error acquiring connection from pool:', err);
             return;
         }
-
         console.log('Connected to the database.');
-
-        // Release the connection back to the pool
         connection.release();
     });
 }
