9316933b149351b7320ce08e4530c34a1233e48f
Rehan Shah <flyrehan786@gmail.com>
2023-08-11 17:49:30 +0500

    cleaning code.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,5 @@
 const Joi = require('joi');
 const bcrypt = require('bcrypt');
-const _ = require('lodash');
-const {User} = require('../models/user');
-const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
@@ -25,8 +22,6 @@ function validate(req) {
     email: Joi.string().min(5).max(255).required().email(),
     password: Joi.string().min(5).max(255).required()
   };
-
   return Joi.validate(req, schema);
 }
-
 module.exports = router; 
