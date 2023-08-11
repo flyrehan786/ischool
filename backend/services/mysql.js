99a4e5a5fe6e3ee711df146776a5353826022c03
Rehan Shah <flyrehan786@gmail.com>
2023-08-11 12:43:05 +0500

    updating mysql service.

--- backend/services/mysql.js
+++ backend/services/mysql.js
@@ -1,8 +1,24 @@
 let mysql2 = require('mysql2');
-let connection = mysql2.createPool({
-  host:"localhost",
-  user: "root",
-  password: "root",
-  database: "school_db"
+let pool = mysql2.createPool({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "school_db"
 });
-module.exports.db = connection;
+// Connect to the database
+function checkConnection() {
+    // Acquire a connection from the pool
+    pool.getConnection((err, connection) => {
+        if (err) {
+            console.error('Error acquiring connection from pool:', err);
+            return;
+        }
+
+        console.log('Connected to the database.');
+
+        // Release the connection back to the pool
+        connection.release();
+    });
+}
+module.exports.db_connection = checkConnection;
+module.exports.db = pool;
