10323d795849afe22b32ec1547291710efac79bf
Rehan Shah <flyrehan786@gmail.com>
2023-08-11 12:46:03 +0500

    cleaning code.

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
-const dbConnection = require('./services/mysql').db_connection;
+const { db_connection } = require('./services/mysql');
 const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
     console.log(`Listening on port ${port}...`)
-    dbConnection();
+    db_connection();
 });
 
 module.exports = server;
