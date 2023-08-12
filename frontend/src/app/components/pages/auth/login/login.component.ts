b9cc5de883ce2af916e9aecc197713b7b7c417d0
Rehan Shah <flyrehan786@gmail.com>
2023-08-13 01:05:12 +0500

    updating login form. And toast core component.

--- frontend/src/app/components/pages/auth/login/login.component.ts
+++ frontend/src/app/components/pages/auth/login/login.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { IControl } from 'src/app/components/core/components/form/deps/IControl';
 import { TYPE_text, TYPE_password } from 'src/app/components/core/components/form/deps/control-types';
 import { VALIDATION_MESSAGES } from 'src/app/components/core/components/form/deps/validation-messages';
+import { ToastComponent } from 'src/app/components/core/components/toast/toast/toast.component';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +10,7 @@ import { VALIDATION_MESSAGES } from 'src/app/components/core/components/form/dep
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  @ViewChild(ToastComponent) toastComponent: ToastComponent;
   config: IControl[] = [
     {
       type: TYPE_text,
@@ -17,10 +19,10 @@ export class LoginComponent implements OnInit {
         [
           { key: 'required', value: 'required', message: VALIDATION_MESSAGES.required },
           { key: 'minLength', value: '5', message: VALIDATION_MESSAGES.minlength(5) },
-          { key: 'maxLength', value: '30', message: VALIDATION_MESSAGES.maxlength(30) },
+          { key: 'maxLength', value: '45', message: VALIDATION_MESSAGES.maxlength(30) },
         ],
         visible: true,
-        bsCols: 'col-md-3'
+        bsCols: 'col-md-7'
     },
     {
       type: TYPE_password,
@@ -28,14 +30,24 @@ export class LoginComponent implements OnInit {
       validators:
         [
           { key: 'required', value: 'required', message: VALIDATION_MESSAGES.required },
+          { key: 'minLength', value: '5', message: VALIDATION_MESSAGES.minlength(5) },
+          { key: 'maxLength', value: '45', message: VALIDATION_MESSAGES.maxlength(30) },
         ],
         visible: true,
-        bsCols: 'col-md-12'
+        bsCols: 'col-md-7'
     },
   ];
   constructor() { }
   ngOnInit(): void {
   }
   onSubmit(e) {
+    console.log(e);
+    const username = e.username;
+    const password = e.password;
+
+    const payload = { username, password };
+    console.log('Login Payload::');
+    console.log(payload);
+    this.toastComponent.show
   }
 }
