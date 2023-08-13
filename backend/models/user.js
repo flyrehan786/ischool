075a86dfbe7c4ff99c2e5d00e00e70a4bf0ca77e
Rehan Shah <flyrehan786@gmail.com>
2023-08-13 14:33:52 +0500

    reformating user model.

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,50 +4,6 @@ const db = require('../services/mysql').db;
 const bcrypt = require("bcrypt");
 const config = require('../config/default.json');
 
-async function findUser(username) {
-  return new Promise((resolve, reject) => {
-    db.execute(`SELECT * FROM users WHERE username=?`, [username], (err, result) => {
-      if (err) reject(err);
-      if (result.length > 0) resolve(result);
-      else resolve(null);
-    });
-  })
-}
-async function saveUser(newUser) {
-  return new Promise((resolve, reject) => {
-    db.execute(`INSERT INTO users VALUES(default, ?, ?, ?, ?, ?, ?, NOW(), NOW(), 1, 1)`, [ newUser.first_name, newUser.last_name, newUser.email, newUser.username, newUser.password, newUser.is_admin ], (err, result) => {
-      if (err) reject(err);
-      db.execute(`SELECT id FROM users WHERE id = LAST_INSERT_ID();`, (err, result) => {
-        if (err) reject(err);
-        if (result.length > 0) resolve(result[0].id);
-        else resolve(null);
-      })
-    });
-  })
-}
-
-function generateAuthToken(newUser) {
-  const token = jwt.sign(
-    {
-      id: newUser.id,
-      first_name: newUser.first_name,
-      last_name: newUser.last_name,
-      email: newUser.email,
-      username: newUser.username,
-      isAdmin: newUser.is_admin
-    },
-    config.jwtPrivateKey, {
-      expiresIn: 3000
-    }
-  );
-  return token;
-};
-
-async function encryptPassword(password) {
-  const salt = await bcrypt.genSalt(10);
-  const encryptedPassword = await bcrypt.hash(password, salt);
-  return encryptedPassword;
-}
 function validateUser(user) {
   const schema = {
     first_name: Joi.string()
@@ -79,9 +35,51 @@ function validateUser(user) {
 
   return Joi.validate(user, schema);
 }
+async function findUser(username) {
+  return new Promise((resolve, reject) => {
+    db.execute(`SELECT * FROM users WHERE username=?`, [username], (err, result) => {
+      if (err) reject(err);
+      if (result.length > 0) resolve(result);
+      else resolve(null);
+    });
+  })
+}
+async function saveUser(newUser) {
+  return new Promise((resolve, reject) => {
+    db.execute(`INSERT INTO users VALUES(default, ?, ?, ?, ?, ?, ?, NOW(), NOW(), 1, 1)`, [ newUser.first_name, newUser.last_name, newUser.email, newUser.username, newUser.password, newUser.is_admin ], (err, result) => {
+      if (err) reject(err);
+      db.execute(`SELECT id FROM users WHERE id = LAST_INSERT_ID();`, (err, result) => {
+        if (err) reject(err);
+        if (result.length > 0) resolve(result[0].id);
+        else resolve(null);
+      })
+    });
+  })
+}
+async function encryptPassword(password) {
+  const salt = await bcrypt.genSalt(10);
+  const encryptedPassword = await bcrypt.hash(password, salt);
+  return encryptedPassword;
+}
+function generateAuthToken(newUser) {
+  const token = jwt.sign(
+    {
+      id: newUser.id,
+      first_name: newUser.first_name,
+      last_name: newUser.last_name,
+      email: newUser.email,
+      username: newUser.username,
+      isAdmin: newUser.is_admin
+    },
+    config.jwtPrivateKey, {
+      expiresIn: 3000
+    }
+  );
+  return token;
+};
 
-exports.generateAuthToken = generateAuthToken;
 exports.validate = validateUser;
 exports.findUser = findUser;
 exports.encryptedPassword = encryptPassword;
 exports.saveUser = saveUser;
+exports.generateAuthToken = generateAuthToken;
