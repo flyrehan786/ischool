f64d27af4e01424548a523ff21d242515bbc0931
Rehan Shah <flyrehan786@gmail.com>
2023-08-17 00:21:08 +0500

    updating teachers model.

--- backend/models/teachers.js
+++ backend/models/teachers.js
@@ -1,24 +1,5 @@
 const Joi = require('joi');
-const mongoose = require('mongoose');
-
-const Teacher = mongoose.model('Teacher', new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 50
-  },
-  isGold: {
-    type: Boolean,
-    default: false
-  },
-  phone: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 50
-  }
-}));
+const db = require('../services/mysql').db;
 
 function validateTeacher(teacher) {
   const schema = {
@@ -29,6 +10,44 @@ function validateTeacher(teacher) {
 
   return Joi.validate(teacher, schema);
 }
+async function findAll() {
+  return new Promise((resolve, reject) => {
+    db.execute((`SELECT * FROM teachers`), [], (err, result) => {
+      if(err) reject(err);
+      if(result.length > 0) resolve(result);
+      else resolve([]);
+    })
+  })
+}
+async function findTeacher(id) {
+  return new Promise((resolve, reject) => {
+    db.execute(`SELECT * FROM teachers WHERE id=?`, [id], (err, result) => {
+      if (err) reject(err);
+      if (result.length > 0) resolve(result[0]);
+      else resolve(null);
+    });
+  })
+}
+async function saveTeacher(newTeacher) {
+  return new Promise((resolve, reject) => {
+    db.execute(`INSERT INTO teachers VALUES(default, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW(), 1, 1)`, [ newTeacher.first_name, newTeacher.last_name, newTeacher.email, newTeacher.username, newTeacher.password, newTeacher.is_admin, newTeacher.status ], (err, result) => {
+      if (err) reject(err);
+      db.execute(`SELECT id FROM teachers WHERE id = LAST_INSERT_ID();`, (err, result) => {
+        if (err) reject(err);
+        if (result.length > 0) resolve(result[0].id);
+        else resolve(null);
+      })
+    });
+  })
+}
+async function updateTeacher(id, updatedTeacher) {}
+async function deleteTeacher(id) {}
+async function deActivateTeacher(id) {}
 
-exports.Teacher = Teacher; 
 exports.validate = validateTeacher;
+exports.findAll = findAll;
+exports.findTeacher = findTeacher;
+exports.saveTeacher = saveTeacher; 
+exports.updateTeacher = updateTeacher;
+exports.deleteTeacher = deleteTeacher;
+exports.deActivateTeacher = deActivateTeacher;
