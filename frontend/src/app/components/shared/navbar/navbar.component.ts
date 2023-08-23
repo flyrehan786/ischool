14ccb1e5d5565e0278a9088984ca8e6e99f9c129
Rehan Shah <flyrehan786@gmail.com>
2023-08-23 23:35:03 +0500

    Updating navigation menus.

--- frontend/src/app/components/shared/navbar/navbar.component.ts
+++ frontend/src/app/components/shared/navbar/navbar.component.ts
@@ -7,14 +7,7 @@ import { AuthService } from 'src/app/services/auth/auth.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  leftSideLinks = [
-    { title: 'Resources', icon: '<i class="bt bt-people-fill"></i>', routerLink: '/students', isAnchor: true, isDropDown: false, isInput: false, dropDownOptions: [] },
-    { title: 'Certificates', icon: '<i class="bi bi-card-checklist"></i>', routerLink: '/certificates', isAnchor: true, isDropDown: false, isInput: false, dropDownOptions: [] },
-    { title: 'Reports', icon: '<i class="bi bi-graph-up"></i>', routerLink: '/reports', isAnchor: true, isDropDown: false, isInput: false, dropDownOptions: [] },
-    { title: 'Notifications', icon: 'i class="bi bi-bell-fill"></i>', routerLink: '/notifications', isAnchor: false, isDropDown: false, isInput: false, dropDownOptions: [] },
-  ];
   constructor(public authService: AuthService) { }
-
   ngOnInit(): void {
   }
 
