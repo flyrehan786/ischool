ee3362aaab11219babc12029fab3965cf092cbe8
Rehan Shah <flyrehan786@gmail.com>
2023-08-24 20:32:31 +0500

    updating certificate form.

--- frontend/src/app/components/pages/certificates/certificate-form/certificate-form/certificate-form.component.ts
+++ frontend/src/app/components/pages/certificates/certificate-form/certificate-form/certificate-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { IControl } from 'src/app/components/core/components/form/deps/IControl';
-import { TYPE_radio, TYPE_text } from 'src/app/components/core/components/form/deps/control-types';
+import { TYPE_radio, TYPE_text, TYPE_textarea } from 'src/app/components/core/components/form/deps/control-types';
 import { VALIDATION_MESSAGES } from 'src/app/components/core/components/form/deps/validation-messages';
 import { ToastComponent } from 'src/app/components/core/components/toast/toast/toast.component';
 
@@ -15,7 +15,7 @@ export class CertificateFormComponent implements OnInit {
   config: IControl[] = [
     {
       type: TYPE_text,
-      key: 'firstname', defaultValue: '',
+      key: 'name', defaultValue: '',
       validators:
         [
           { key: 'required', value: 'required', message: VALIDATION_MESSAGES.required },
@@ -23,11 +23,11 @@ export class CertificateFormComponent implements OnInit {
           { key: 'maxLength', value: '45', message: VALIDATION_MESSAGES.maxlength(45) },
         ],
       visible: true,
-      bsCols: 'col-md-6'
+      bsCols: 'col-md-12'
     },
     {
-      type: TYPE_text,
-      key: 'lastname', defaultValue: '',
+      type: TYPE_textarea,
+      key: 'template', defaultValue: '',
       validators:
         [
           { key: 'required', value: 'required', message: VALIDATION_MESSAGES.required },
@@ -35,107 +35,8 @@ export class CertificateFormComponent implements OnInit {
           { key: 'maxLength', value: '45', message: VALIDATION_MESSAGES.maxlength(45) },
         ],
       visible: true,
-      bsCols: 'col-md-6'
-    },
-    {
-      type: TYPE_radio,
-      key: 'gender', defaultValue: '',
-      options: [
-        { key: '  Yes', value: '0' },
-        { key: '  No', value: '1' },
-      ],
-      validators:
-        [
-          { key: 'required', value: 'required', message: VALIDATION_MESSAGES.required },
-        ],
-      visible: true,
-      bsCols: 'col-md-6'
-    },
-    {
-      type: TYPE_text,
-      key: 'cnic', defaultValue: '',
-      label: 'CNIC',
-      validators:
-        [
-          { key: 'required', value: 'required', message: VALIDATION_MESSAGES.required },
-          { key: 'minLength', value: '2', message: VALIDATION_MESSAGES.minlength(5) },
-          { key: 'maxLength', value: '45', message: VALIDATION_MESSAGES.maxlength(45) },
-        ],
-      visible: true,
-      bsCols: 'col-md-6'
-    },
-    {
-      type: TYPE_text,
-      key: 'age', defaultValue: '',
-      validators:
-        [
-          { key: 'required', value: 'required', message: VALIDATION_MESSAGES.required },
-          { key: 'minLength', value: '1', message: VALIDATION_MESSAGES.minlength(1) },
-          { key: 'maxLength', value: '3', message: VALIDATION_MESSAGES.maxlength(3) },
-        ],
-      visible: true,
-      bsCols: 'col-md-6'
-    },
-    {
-      type: TYPE_text,
-      key: 'father_name', defaultValue: '',
-      validators:
-        [
-          { key: 'required', value: 'required', message: VALIDATION_MESSAGES.required },
-          { key: 'minLength', value: '2', message: VALIDATION_MESSAGES.minlength(5) },
-          { key: 'maxLength', value: '45', message: VALIDATION_MESSAGES.maxlength(45) },
-        ],
-      visible: true,
-      bsCols: 'col-md-6'
-    },
-    {
-      type: TYPE_text,
-      key: 'father_cnic', defaultValue: '',
-      validators:
-        [
-          { key: 'required', value: 'required', message: VALIDATION_MESSAGES.required },
-          { key: 'minLength', value: '5', message: VALIDATION_MESSAGES.minlength(5) },
-          { key: 'maxLength', value: '45', message: VALIDATION_MESSAGES.maxlength(45) },
-        ],
-      visible: true,
-      bsCols: 'col-md-6'
-    },
-    {
-      type: TYPE_text,
-      key: 'post_office', defaultValue: '',
-      validators:
-        [
-          { key: 'required', value: 'required', message: VALIDATION_MESSAGES.required },
-          { key: 'minLength', value: '5', message: VALIDATION_MESSAGES.minlength(5) },
-          { key: 'maxLength', value: '45', message: VALIDATION_MESSAGES.maxlength(45) },
-        ],
-      visible: true,
-      bsCols: 'col-md-6'
-    },
-    {
-      type: TYPE_text,
-      key: 'tehsil', defaultValue: '',
-      validators:
-        [
-          { key: 'required', value: 'required', message: VALIDATION_MESSAGES.required },
-          { key: 'minLength', value: '5', message: VALIDATION_MESSAGES.minlength(5) },
-          { key: 'maxLength', value: '45', message: VALIDATION_MESSAGES.maxlength(45) },
-        ],
-      visible: true,
-      bsCols: 'col-md-6'
-    },
-    {
-      type: TYPE_text,
-      key: 'district', defaultValue: '',
-      validators:
-        [
-          { key: 'required', value: 'required', message: VALIDATION_MESSAGES.required },
-          { key: 'minLength', value: '5', message: VALIDATION_MESSAGES.minlength(5) },
-          { key: 'maxLength', value: '45', message: VALIDATION_MESSAGES.maxlength(45) },
-        ],
-      visible: true,
-      bsCols: 'col-md-6'
-    },
+      bsCols: 'col-md-12'
+    }
   ];
   constructor() { }
 
