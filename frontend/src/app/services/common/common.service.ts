2166416488822acdbd4459e30a2128d7b8a3723a
Rehan Shah <flyrehan786@gmail.com>
2023-08-31 12:52:45 +0500

    implementing subject in common serivce.

--- frontend/src/app/services/common/common.service.ts
+++ frontend/src/app/services/common/common.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommonService {
-  
+  private dynamicEventSubject = new Subject<any>();
   constructor() { }
-
+  publishEvent(eventData: any) {
+    this.dynamicEventSubject.next(eventData);
+  }
   getKeys(obj) {
     const keys = Object.keys(obj);
     return keys;
