f4851fdc0ea09c8828460b3c09a020c8775f34ec
Rehan Shah <flyrehan786@gmail.com>
2023-09-01 12:51:33 +0500

    code refactoring.

--- backend/routes/students.js
+++ backend/routes/students.js
@@ -19,7 +19,6 @@ router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
   const student = await saveStudent(req.body);
-  console.log(student);
   res.send(student);
 });
 
