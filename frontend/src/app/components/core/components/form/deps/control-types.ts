d2102ff68d185cec15dc8dab0158d3ada32ec076
Rehan Shah <flyrehan786@gmail.com>
2023-09-02 23:33:46 +0500

    adding 'date' type support in core form.

--- frontend/src/app/components/core/components/form/deps/control-types.ts
+++ frontend/src/app/components/core/components/form/deps/control-types.ts
@@ -1,4 +1,5 @@
 export const TYPE_text = 'text';
+export const TYPE_date = 'date';
 export const TYPE_password = 'password';
 export const TYPE_textarea = 'textarea';
 export const TYPE_radio = 'radio';
