b1ef088d69e3cc4b51e39f5d56e5c1943fe4e057
Rehan Shah <flyrehan786@gmail.com>
2023-09-02 22:40:50 +0500

    updating teachers api's.

--- backend/routes/teachers.js
+++ backend/routes/teachers.js
@@ -1,70 +1,94 @@
-const { Teacher, validate } = require("../models/teachers");
+const { 
+  validate, 
+  findAll, 
+  findTeacher,
+  deleteTeacher,
+  updateTeacher,
+  activateTeacher,
+  deActivateTeacher,
+  saveTeacher} = require("../models/teachers");
 const auth = require("../middleware/auth");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", auth, async (req, res) => {
-  const teachers = await Teacher.find()
-    .select("-__v")
-    .sort("name");
+router.get("/", async (req, res) => {
+  const teachers = await findAll();
+  teachers.forEach(s => {
+    s.created_at = new Date(s.created_at).toLocaleString();
+    s.updated_at = new Date(s.updated_at).toLocaleString();
+  })
   res.send(teachers);
 });
 
-router.post("/", auth, async (req, res) => {
+router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-
-  let teacher = new Teacher({
-    name: req.body.name,
-    isGold: req.body.isGold,
-    phone: req.body.phone
-  });
-  teacher = await teacher.save();
-
-  res.send(teacher);
+  const createdTeacher = await saveTeacher(req.body);
+  res.send(createdTeacher);
 });
-
-router.put("/:id", auth, async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  const teacher = await Teacher.findByIdAndUpdate(
+router.put("/disable/:id", async (req, res) => {
+  const rowsAffected = await deActivateTeacher(
     req.params.id,
-    {
-      name: req.body.name,
-      isGold: req.body.isGold,
-      phone: req.body.phone
-    },
-    { new: true }
+    req.body
   );
 
+  if (rowsAffected == false) {
+    console.log(404);
+    return res
+    .status(404)
+    .send("The teacher with the given ID was not found.");
+  }
+  
+  res.send({ updated: true });
+});
+router.put("/activate/:id", async (req, res) => {
+  const rowsAffected = await activateTeacher(
+    req.params.id,
+    req.body
+  );
+
+  if (rowsAffected == false) {
+    return res
+    .status(404)
+    .send("The teacher with the given ID was not found.");
+  }
+  
+  res.send({ updated: true });
+});
+router.put("/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const updatedTeacher = await updateTeacher(
+    req.params.id,
+    req.body
+  );
+
+  if (!updatedTeacher)
+    return res
+      .status(404)
+      .send("The teacher with the given ID was not found.");
+  res.send(updatedTeacher);
+});
+router.delete("/:id", async (req, res) => {
+  const rowsAffected = await deleteTeacher(req.params.id);
+  if (rowsAffected == false) {
+    return res
+    .status(404)
+    .send("The teacher with the given ID was not found.");
+  }
+  
+  res.send({ deleted: true });
+});
+router.get("/:id", async (req, res) => {
+  const teacher = await findTeacher(req.params.id);
   if (!teacher)
     return res
       .status(404)
       .send("The teacher with the given ID was not found.");
 
-  res.send(teacher);
-});
-
-router.delete("/:id", auth, async (req, res) => {
-  const teacher = await Teacher.findByIdAndRemove(req.params.id);
-
-  if (!teacher)
-    return res
-      .status(404)
-      .send("The student with the given ID was not found.");
-
-  res.send(teacher);
-});
-
-router.get("/:id", auth, async (req, res) => {
-  const teacher = await Teacher.findById(req.params.id).select("-__v");
-
-  if (!teacher)
-    return res
-      .status(404)
-      .send("The teacher with the given ID was not found.");
-
+  teacher.created_at = new Date(teacher.created_at).toLocaleString();
+  teacher.updated_at = new Date(teacher.updated_at).toLocaleString();
   res.send(teacher);
 });
 
