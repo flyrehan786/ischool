b078fadc1a342ffb5438313a67e9e567eaac37de
Rehan Shah <flyrehan786@gmail.com>
2023-09-02 22:34:22 +0500

    updating updateTeacher function in teachers model.

--- backend/models/teachers.js
+++ backend/models/teachers.js
@@ -14,8 +14,8 @@ function validateTeacher(teacher) {
 async function findAll() {
   return new Promise((resolve, reject) => {
     db.execute((`SELECT * FROM teachers`), [], (err, result) => {
-      if(err) reject(err);
-      if(result.length > 0) resolve(result);
+      if (err) reject(err);
+      if (result.length > 0) resolve(result);
       else resolve([]);
     })
   })
@@ -31,24 +31,43 @@ async function findTeacher(id) {
 }
 async function saveTeacher(newTeacher) {
   return new Promise((resolve, reject) => {
-    db.execute(`INSERT INTO teachers VALUES(default, ?, ?, ?, ?, ?, 1 , NOW(), NOW(), 1, 1)`, 
-    [ 
-      newTeacher.first_name, 
-      newTeacher.last_name, 
-      newTeacher.qualification, 
-      newTeacher.designation, 
-      newTeacher.joining_date, 
-    ], (err, result) => {
-      if (err) reject(err);
-      db.execute(`SELECT id FROM teachers WHERE id = LAST_INSERT_ID();`, (err, result) => {
+    db.execute(`INSERT INTO teachers VALUES(default, ?, ?, ?, ?, ?, 1 , NOW(), NOW(), 1, 1)`,
+      [
+        newTeacher.first_name,
+        newTeacher.last_name,
+        newTeacher.qualification,
+        newTeacher.designation,
+        newTeacher.joining_date,
+      ], (err, result) => {
         if (err) reject(err);
-        if (result.length > 0) resolve(result[0].id);
-        else resolve(null);
-      })
-    });
+        db.execute(`SELECT id FROM teachers WHERE id = LAST_INSERT_ID();`, (err, result) => {
+          if (err) reject(err);
+          if (result.length > 0) resolve(result[0].id);
+          else resolve(null);
+        })
+      });
   })
 }
-async function updateTeacher(id, updatedTeacher) {}
+async function updateTeacher(id, updatedTeacher) {
+  return new Promise((resolve, reject) => {
+    db.execute(`Update students SET first_name=?, last_name=?, gender=?, cnic=?, age=?, father_name=?, father_cnic=?, post_office=?, tehsil=?, district=? WHERE id=?;`,
+      [
+        updatedTeacher.first_name,
+        updatedTeacher.last_name,
+        updatedTeacher.qualification,
+        updatedTeacher.designation,
+        updatedTeacher.joining_date,
+        id
+      ], (err, result) => {
+        if (err) reject(err);
+        db.execute(`SELECT * FROM students WHERE id = ${id};`, (err, result) => {
+          if (err) reject(err);
+          if (result.length > 0) resolve(result[0]);
+          else resolve(null);
+        })
+      })
+  })
+}
 async function deleteTeacher(id) {
   return new Promise((resolve, reject) => {
     db.execute(`DELETE FROM teachers WHERE id = ${id};`, (err, result) => {
@@ -56,11 +75,11 @@ async function deleteTeacher(id) {
       if (result.affectedRows == 1) resolve(true);
       else resolve(false);
     })
-    });
- }
-async function deActivateTeacher(id) { 
+  });
+}
+async function deActivateTeacher(id) {
   console.log(id);
-  return new Promise((resolve,reject) => {
+  return new Promise((resolve, reject) => {
     db.execute(`UPDATE teachers SET status=? WHERE id=?`, [0, id], (err, result) => {
       console.log(result);
       if (err) reject(err);
@@ -69,9 +88,9 @@ async function deActivateTeacher(id) {
     })
   })
 }
-async function activateTeacher(id) { 
+async function activateTeacher(id) {
   console.log(id);
-  return new Promise((resolve,reject) => {
+  return new Promise((resolve, reject) => {
     db.execute(`UPDATE teachers SET status=? WHERE id=?`, [1, id], (err, result) => {
       console.log(result);
       if (err) reject(err);
@@ -83,7 +102,7 @@ async function activateTeacher(id) {
 exports.validate = validateTeacher;
 exports.findAll = findAll;
 exports.findTeacher = findTeacher;
-exports.saveTeacher = saveTeacher; 
+exports.saveTeacher = saveTeacher;
 exports.updateTeacher = updateTeacher;
 exports.deleteTeacher = deleteTeacher;
 exports.deActivateTeacher = deActivateTeacher;
