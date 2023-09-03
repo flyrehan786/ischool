432c3604695f99f08de78a848bd43b3948d61126
Rehan Shah <flyrehan786@gmail.com>
2023-09-03 23:21:32 +0500

    working on certificate module.

--- frontend/src/app/components/pages/certificates/certificate-details/certificate-details/certificate-details.component.ts
+++ frontend/src/app/components/pages/certificates/certificate-details/certificate-details/certificate-details.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastComponent } from 'src/app/components/core/components/toast/toast/toast.component';
+import { CertificatesService } from 'src/app/services/certificates/certificates.service';
 
 @Component({
   selector: 'app-certificate-details',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./certificate-details.component.css']
 })
 export class CertificateDetailsComponent implements OnInit {
-
-  constructor() { }
-
+  certificateId;
+  certificate;
+  @ViewChild(ToastComponent) toastComponent: ToastComponent;
+  constructor(private _certificateService: CertificatesService, private _route: ActivatedRoute, private _router: Router) { }
   ngOnInit(): void {
+    this.certificate = +this._route.snapshot.paramMap.get('id');
+    this.getCertificate();
   }
-
+  getCertificate() {
+    this._certificateService.getCertificate(this.certificateId).subscribe(certificate => {
+      this.certificate = certificate;
+    },
+    (error) => {
+      console.error('An error occurred:', error);
+      this.toastComponent.show('(Updating Certificate API Failed).', false, true, false);
+    })
+  }
+  deleteCertificate() {
+    if(confirm('Are you sure you want to delete this certificate?')) {
+      this._certificateService.deleteCertificate(this.certificate).subscribe(res => {
+        this._router.navigateByUrl('/certificates');
+      },
+      (error) => {
+        console.error('An error occurred:', error);
+        this.toastComponent.show('(Updating Certificate API Failed).', false, true, false);
+      });
+    }
+  }
 }
--- frontend/src/app/components/pages/certificates/certificates/certificates.component.ts
+++ frontend/src/app/components/pages/certificates/certificates/certificates.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CertificatesService } from 'src/app/services/certificates/certificates.service';
+import { CommonService } from 'src/app/services/common/common.service';
 
 @Component({
   selector: 'app-certificates',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./certificates.component.css']
 })
 export class CertificatesComponent implements OnInit {
-
-  constructor() { }
-
+  eventLabel = '_certificate_event';
+  title;
+  headers;
+  rows;
+  filters = [ 'name' ];
+  constructor(private _certificatesService: CertificatesService, private _commonService: CommonService, private _router: Router) { }
   ngOnInit(): void {
+    this.getCertificates();
+    this._commonService.getEvent().subscribe(e => {
+      if(e.event == this.eventLabel) {
+        this._router.navigateByUrl("/certifcate/details/" + e.id);
+      }
+    })
   }
-
+  getCertificates() {
+    this._certificatesService.getCertificates().subscribe(res => {
+      this.rows = res;
+    });
+  }
 }
