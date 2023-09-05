09237e0a7415eb568a85932f90686ef7ef9d6f05
Rehan Shah <flyrehan786@gmail.com>
2023-09-05 23:45:38 +0500

    ...

--- frontend/src/app/components/pages/exam-types/types/types.component.ts
+++ frontend/src/app/components/pages/exam-types/types/types.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonService } from 'src/app/services/common/common.service';
+import { ExamTypeService } from 'src/app/services/exam-type/exam-type.service';
 
 @Component({
   selector: 'app-types',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./types.component.css']
 })
 export class TypesComponent implements OnInit {
-
-  constructor() { }
-
+  eventLabel = '_exam_type_event';
+  title = 'Exam Types';
+  headers = [ 'id', 'name', 'created_at', 'updated_at'];
+  rows;
+  filters = [ 'name' ];
+  constructor(private _examTypeService: ExamTypeService, private _commonService: CommonService, private _router: Router) { }
   ngOnInit(): void {
+    this.getExamTypes();
+    this._commonService.getEvent().subscribe(e => {
+      if(e.event == this.eventLabel) {
+        this._router.navigateByUrl("/exam-type/details/" + e.id);
+      }
+    })
   }
-
+  getExamTypes() {
+    this._examTypeService.getExamTypes().subscribe(res => {
+      this.rows = res;
+    });
+  }
 }
