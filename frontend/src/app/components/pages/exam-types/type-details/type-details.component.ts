035ebf84bcc6c13d8c8fa07f82671fb13edad392
Rehan Shah <flyrehan786@gmail.com>
2023-09-05 23:42:28 +0500

    ...

--- frontend/src/app/components/pages/exam-types/type-details/type-details.component.ts
+++ frontend/src/app/components/pages/exam-types/type-details/type-details.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastComponent } from 'src/app/components/core/components/toast/toast/toast.component';
+import { ExamTypeService } from 'src/app/services/exam-type/exam-type.service';
 
 @Component({
   selector: 'app-type-details',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./type-details.component.css']
 })
 export class TypeDetailsComponent implements OnInit {
-
-  constructor() { }
-
+  examTypeId;
+  examType;
+  @ViewChild(ToastComponent) toastComponent: ToastComponent;
+  constructor(private _examTypeService: ExamTypeService, private _route: ActivatedRoute, private _router: Router) { }
   ngOnInit(): void {
+    this.examTypeId = +this._route.snapshot.paramMap.get('id');
+    this.getExamType();
   }
-
+  getExamType() {
+    this._examTypeService.getExamType(this.examTypeId).subscribe(et => {
+      this.examType = et;
+    },
+    (error) => {
+      console.error('An error occurred:', error);
+      this.toastComponent.show('(Updating Exam-Type API Failed).', false, true, false);
+    })
+  }
+  deleteExamType() {
+    if(confirm('Are you sure you want to delete this exam-type?')) {
+      this._examTypeService.deleteExamType(this.examTypeId).subscribe(res => {
+        this._router.navigateByUrl('/exam-types');
+      },
+      (error) => {
+        console.error('An error occurred:', error);
+        this.toastComponent.show('(Updating Exam-Type API Failed).', false, true, false);
+      });
+    }
+  }
 }
