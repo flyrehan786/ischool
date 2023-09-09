0473da4f5f83a9515f1f8bdacc7b7b1b75bf39e8
Rehan Shah <flyrehan786@gmail.com>
2023-09-09 23:54:49 +0500

    implementing exam-form component.

--- frontend/src/app/components/pages/exams/exam-form/exam-form/exam-form.component.ts
+++ frontend/src/app/components/pages/exams/exam-form/exam-form/exam-form.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IControl } from 'src/app/components/core/components/form/deps/IControl';
+import { TYPE_text } from 'src/app/components/core/components/form/deps/control-types';
+import { VALIDATION_MESSAGES } from 'src/app/components/core/components/form/deps/validation-messages';
+import { ToastComponent } from 'src/app/components/core/components/toast/toast/toast.component';
+import { ExamsService } from 'src/app/services/exams/exams.service';
 
 @Component({
   selector: 'app-exam-form',
@@ -6,10 +13,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exam-form.component.css']
 })
 export class ExamFormComponent implements OnInit {
-
-  constructor() { }
+  examId;
+  editMode = false;
+  exam;
+  formTitle = 'New Exam';
+  @ViewChild(ToastComponent) toastComponent: ToastComponent;
+  config: IControl[] = [
+    {
+      type: TYPE_text,
+      key: 'name', defaultValue: '', label: 'Name',
+      validators:
+        [
+          { key: 'required', value: 'required', message: VALIDATION_MESSAGES.required },
+          { key: 'minLength', value: '5', message: VALIDATION_MESSAGES.minlength(5) },
+          { key: 'maxLength', value: '45', message: VALIDATION_MESSAGES.maxlength(45) },
+        ],
+      visible: true,
+      bsCols: 'col-md-2'
+    },
+  ];
+  constructor(
+    private _route: ActivatedRoute, 
+    private _examService: ExamsService, 
+    private _router: Router) { }
 
   ngOnInit(): void {
+    this.examId = this._route.snapshot.paramMap.get('id');
+    if (this.examId) {
+      this.editMode = true;
+      this.formTitle = 'Edit Exam';
+      this.getExam();
+    }
   }
-
+  getExam() {
+    this._examService.getExam(this.examId).subscribe(exam => {
+      this.exam = exam;
+      this.config.forEach(c => {
+        if (c.key == 'name') c.defaultValue = this.exam.name;
+      })
+    })
+  }
+  onSubmit(e) {
+    if (this.editMode) {
+      this._examService.putExam(this.examId, e).subscribe(
+        (res: HttpResponse<any>) => {
+          this.toastComponent.show('(Exam Updated Successfully).', true, false, false);
+          setTimeout(() => {
+            this._router.navigateByUrl('/exams');
+          }, 1500)
+        },
+        (error) => {
+          console.error('An error occurred:', error);
+          this.toastComponent.show('(Updating Exam API Failed).', false, true, false);
+        })
+    }
+    else {
+      this._examService.postExam(e).subscribe(
+        (res: HttpResponse<any>) => {
+          this.toastComponent.show('(Exam Created Successfully).', true, false, false);
+          setTimeout(() => {
+            this._router.navigateByUrl('/exam');
+          }, 1500);
+        },
+        (error) => {
+          console.error('An error occurred:', error);
+          this.toastComponent.show('(Adding Exam API Failed).', false, true, false);
+        })
+    }
+  }
 }
