a38cf591bc3d72f561639fd4a694c41511bcd8d5
Rehan Shah <flyrehan786@gmail.com>
2023-10-11 17:40:40 +0500

    adding db change log scripts.

---
+++ db-change-logs/school_db_student_enrollments.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: localhost    Database: school_db
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `student_enrollments`
+--
+
+DROP TABLE IF EXISTS `student_enrollments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student_enrollments` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `student_id` int DEFAULT NULL,
+  `grade_id` int DEFAULT NULL,
+  `status` varchar(1) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_by` int DEFAULT NULL,
+  `updated_by` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student_enrollments`
+--
+
+LOCK TABLES `student_enrollments` WRITE;
+/*!40000 ALTER TABLE `student_enrollments` DISABLE KEYS */;
+INSERT INTO `student_enrollments` VALUES (4,13,11,'1','2023-09-15 00:16:02','2023-09-15 00:16:02',1,1),(7,17,9,'0','2023-09-15 15:20:06','2023-09-15 15:20:06',1,1),(8,17,17,'0','2023-09-15 15:20:20','2023-09-15 15:20:20',1,1),(9,13,12,'0','2023-09-18 23:14:43','2023-09-18 23:14:43',1,1),(10,13,17,'0','2023-09-18 23:14:53','2023-09-18 23:14:53',1,1),(11,14,6,'0','2023-09-24 15:43:18','2023-09-24 15:43:18',1,1),(12,14,13,'1','2023-09-24 15:43:36','2023-09-24 15:43:36',1,1);
+/*!40000 ALTER TABLE `student_enrollments` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-10-11 17:39:55
